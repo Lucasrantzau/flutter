a07d5eab098d55564061b7602a604a37cb5fc7df
Lucas Rantzau Schimmel <Lucasrantzau@gmail.com>
2016-03-29 19:15:03 +0200

    Added Post model. Displaying posts correctly by user in their own profiles, and looping trough all posts in time order on homepage.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,20 +7,24 @@ class PagesController < ApplicationController
 
   # back-end code for pages/home
   def home
+    @posts = Post.all
   end
 
-  # back-end code for pages/profile
+   # back-end code for pages/profile
   def profile
-    # Grab the username from the URL as :id
+    # grab the username from the URL as :id
     if (User.find_by_username(params[:id]))
       @username = params[:id]
-    else
-      redirect_to root_path, :notice=> "User not found"
+    else 
+      # redirect to 404 (root for now)
+      redirect_to root_path, :notice=> "User not found!" 
     end
 
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   # back-end code for pages/explore
   def explore
+    @posts = Post.all
   end
 end
